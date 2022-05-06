680250961345d8b530dcac2dd04f17deeb23cc59
Subareeshkrishnan <subareeshkrishnan@gmail.com>
2022-05-06 23:05:46 +0530

    test-report-junit5-maven

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,3 +1,5 @@
+//Running site plugin to generate site and to view the test report
+//http://localhost:63342/MavenTesting/target/site/surefire-report.html
 public class HelloWorld {
 
     public String helloWorld() {
