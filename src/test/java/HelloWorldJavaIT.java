b7148382ba945d8162e2ae65806d1d636292e553
Subareeshkrishnan <subareeshkrishnan@gmail.com>
2022-05-07 12:22:50 +0530

    jacoco-report-maven - first commit

--- src/test/java/HelloWorldJavaIT.java
+++ src/test/java/HelloWorldJavaIT.java
@@ -5,5 +5,7 @@ public class HelloWorldJavaIT {
     @Test
     void myFauxIntegrationTest() {
         System.out.println("IT ran successfully!!!");
+        HelloWorld helloWorld = new HelloWorld();
+        assert ("Hello World".equals(helloWorld.helloWorld()));
     }
 }
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,11 +1,12 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class HelloWorldTest {
 
     @Test
     void testHelloWorld() {
+        System.out.println("junit5 ran successfully!!");
         HelloWorld helloWorld = new HelloWorld();
         assertEquals("Hello World", helloWorld.helloWorld());
     }
