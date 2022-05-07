51349f2b6ef32817943df3f848eae70ca2abb032
Subareeshkrishnan <subareeshkrishnan@gmail.com>
2022-05-07 12:00:09 +0530

    failsafe-junit5-maven - first commit

---
+++ src/test/java/HelloWorldJavaIT.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloWorldJavaIT {
+
+    @Test
+    void myFauxIntegrationTest() {
+        System.out.println("IT ran successfully!!!");
+    }
+}
